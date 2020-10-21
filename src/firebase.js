0e716fd2e88f1d16b07e8744dc70ad74360a4f2d
robertvitoriano <robertvitorianodev@gmail.com>
2020-10-21 07:10:13 -0300

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAl5q3EPVsEtxG_0B1_gAViTQRAyXKGY5k",
+  authDomain: "roberts-whatsapp.firebaseapp.com",
+  databaseURL: "https://roberts-whatsapp.firebaseio.com",
+  projectId: "roberts-whatsapp",
+  storageBucket: "roberts-whatsapp.appspot.com",
+  messagingSenderId: "389675819845",
+  appId: "1:389675819845:web:0ce8c4d022d993f7a4142c",
+  measurementId: "G-X4DVCG8Z7M"
+};
